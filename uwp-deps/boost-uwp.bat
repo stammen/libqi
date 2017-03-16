c55bea0f26b771eb54ef1c36835b460bb7ed34e1
Dale Stammen <dalestam@microsoft.com>
2017-03-15 22:00:29 -0700

    fixed boost script

--- uwp-deps/boost-uwp.bat
+++ uwp-deps/boost-uwp.bat
@@ -15,43 +15,44 @@ mkdir boost-1.60.0
 mkdir boost-build
 pushd boost-build
   git clone https://github.com/boostorg/boost.git
-  cd boost
-  git checkout boost-1.60.0
-  git submodule update --init
   pushd boost
     git checkout boost-1.60.0
-    pushd libs
-      pushd filesystem
-        git remote add winrt https://github.com/stammen/filesystem.git
-        git fetch winrt winrt-1.60.0
-        git checkout winrt-1.60.0
+    git submodule update --init
+    pushd boost
+      git checkout boost-1.60.0
+      pushd libs
+        pushd filesystem
+          git remote add winrt https://github.com/stammen/filesystem.git
+          git fetch winrt winrt-1.60.0
+          git checkout winrt-1.60.0
+        popd
+        pushd thread 
+          git remote add winrt https://github.com/stammen/thread.git
+          git fetch winrt winrt-1.60.0
+          git checkout winrt-1.60.0
+        popd
+        pushd uuid 
+          git remote add winrt https://github.com/stammen/uuid.git
+          git fetch winrt winrt-1.60.0
+          git checkout winrt-1.60.0
+        popd
+        pushd program_options  
+          git remote add winrt https://github.com/stammen/program_options .git
+          git fetch winrt winrt-1.60.0
+          git checkout winrt-1.60.0
+        popd
       popd
-      pushd thread 
-        git remote add winrt https://github.com/stammen/thread.git
-        git fetch winrt winrt-1.60.0
-        git checkout winrt-1.60.0
+      pushd tools
+        pushd build
+          git remote add winrt https://github.com/stammen/build.git
+          git fetch winrt winrt-1.60.0
+          git checkout winrt-1.60.0        
+        popd
       popd
-      pushd uuid 
-        git remote add winrt https://github.com/stammen/uuid.git
-        git fetch winrt winrt-1.60.0
-        git checkout winrt-1.60.0
-      popd
-      pushd program_options  
-        git remote add winrt https://github.com/stammen/program_options .git
-        git fetch winrt winrt-1.60.0
-        git checkout winrt-1.60.0
-      popd
+      call Bootstrap
+      call b2 headers
+      call b2 install --prefix=../../boost-1.60.0 --with-chrono --with-filesystem --with-date_time --with-atomic --with-thread --with-system --with-regex --with-atomic --with-program_options toolset=msvc-14.0 windows-api=store link=shared --variant=release
     popd
-    pushd tools
-      pushd build
-        git remote add winrt https://github.com/stammen/build.git
-        git fetch winrt winrt-1.60.0
-        git checkout winrt-1.60.0        
-      popd
-    popd
-    Bootstrap
-    b2 headers
-    b2 install --prefix=../../boost-1.60.0 --with-chrono --with-filesystem --with-date_time --with-atomic --with-thread --with-system --with-regex --with-atomic --with-program_options toolset=msvc-14.0 windows-api=store link=shared --variant=release
   popd
 echo Boost 1.60.0 installed
 popd
